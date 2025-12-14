6d1c90c97bd8f13eb66370b2bfc1888eda6ada29
Angga Dhammika Jaya <anggadj94@gmail.com>
2025-12-14 22:05:57 +0700

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,12 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Payment;
+use Midtrans\Config;
+use Midtrans\Snap;
 
 class PaymentController extends Controller
 {
-    public function index(Request $request)
+    public function __construct()
     {
-        $payments = Payment::latest()->take(100)->get();
-        return response()->json($payments);
+        Config::$serverKey = config('midtrans.server_key');
+        Config::$isProduction = config('midtrans.is_production');
+        Config::$isSanitized = config('midtrans.is_sanitized');
+        Config::$is3ds = config('midtrans.is_3ds');
+    }
+
+    public function payment(Request $request)
+    {
+        $transaction_details = [
+            'order_id'    => 'ORDER-' . time(),
+            'gross_amount' => 10000, // Gunakan angka uji coba (test amount)
+        ];
+        
+        $customer_details = [
+            'first_name' => 'John',
+            'last_name'  => 'Doe',
+            'email'      => 'johndoe@example.com',
+            'phone'      => '081234567890',
+        ];
+
+        $params = [
+            'transaction_details' => $transaction_details,
+            'customer_details' => $customer_details,
+            // ... detail lain seperti item_details bisa ditambahkan
+        ];
+
+        try {
+            // 3. Dapatkan Snap Token
+            $snapToken = Snap::getSnapToken($params);
+            
+            return view('payment', compact('snapToken', 'transaction_details'));
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
