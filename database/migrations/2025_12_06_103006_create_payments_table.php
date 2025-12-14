b89796e165aa5aaec6411d463de46501dc8ff1e5
Angga Dhammika Jaya <anggadj94@gmail.com>
2025-12-14 22:06:08 +0700

    Update 2025_12_06_103006_create_payments_table.php

--- database/migrations/2025_12_06_103006_create_payments_table.php
+++ database/migrations/2025_12_06_103006_create_payments_table.php
@@ -14,12 +14,42 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained('orders')->onDelete('cascade')->unique();
-            $table->enum('payment_method', ['credit_card', 'paypal', 'bank_transfer']);
+            $table->string('midtrans_order_id')->unique();
+            $table->enum('payment_method', [
+                                            "credit_card",
+                                            "bank_transfer",
+                                            "echannel",
+                                            "permata",
+                                            "bca_klikpay",
+                                            "cimb_clicks",
+                                            "danamon_online",
+                                            "bri_epay",
+                                            "gopay",
+                                            "shopeepay",
+                                            "qris",
+                                            "indomaret",
+                                            "alfamart",
+                                            "kredivo",
+                                            "akulaku"
+                                        ]);
             $table->enum('payment_type', ['full', 'partial']);
             $table->integer('payment_total_amount');
             $table->integer('payment_dp_amount')->nullable();
             $table->integer('payment_dp_paid')->nullable();
-            $table->enum('payment_status', ['pending', 'completed', 'failed']);
+            $table->enum('payment_status', [
+                                            "pending",
+                                            "settlement",
+                                            "capture",
+                                            "authorize",
+                                            "deny",
+                                            "cancel",
+                                            "expire",
+                                            "refund",
+                                            "partial_refund",
+                                            "chargeback",
+                                            "partial_chargeback",
+                                            "failure"
+                                        ]);
             $table->datetime('paid_at')->nullable();
             $table->timestamps();
         });
